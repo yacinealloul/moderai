e03dd9ae25d9ca27eee03938d1c2fbf694cf82ac
yacinealloul <40143057+yacinealloul@users.noreply.github.com>
2023-09-09 14:55:50 +0200

    fix back

--- app/(root)/(routes)/dashboard/page.tsx
+++ app/(root)/(routes)/dashboard/page.tsx
@@ -1,55 +1,54 @@
-"use client";
-import React from 'react';
-import { BarChart } from '@mui/x-charts';
+import React from 'react'
+
+type LogType = {
+  id: number;
+  message: string;
+}
 
 const mockData = {
+  dailyUsage: 57,
   totalUsage: 1200,
   endpoint: "https://api.moder.ai/",
   secretKey: "**********XYZ123",
-};
+  logs: [
+    { id: 1, message: "Log 1" },
+    { id: 2, message: "Log 2" },
+    { id: 3, message: "Log 3" },
+    // ...
+  ]
+}
 
 const Dashboard: React.FC = () => {
   return (
-    <div className="p-10 bg-gray-200 min-h-screen font-sans text-gray-900">
-      <h1 className="text-5xl mb-12 font-bold tracking-tight">Dashboard</h1>
+    <div className="p-8 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl mb-4">Dashboard</h1>
       
-      <div className="grid grid-cols-2 gap-10">
-        <div className="bg-white p-8 rounded-xl shadow-lg space-y-6">
-          <h2 className="text-3xl font-semibold tracking-tighter">API Information</h2>
-          <div className="space-y-3">
-            <h3 className="text-xl font-medium">Endpoint</h3>
-            <p className="text-gray-600 text-lg">{mockData.endpoint}</p>
-          </div>
-          <div className="space-y-3">
-            <h3 className="text-xl font-medium">Secret Key</h3>
-            <p className="text-gray-600 text-lg">{mockData.secretKey} <span className="text-sm text-red-500">[Handle with care]</span></p>
-          </div>
+      <div className="flex justify-between mb-8">
+        <div className="bg-white p-6 rounded-lg shadow-md w-1/2 space-y-4">
+          <h2 className="text-xl">API Endpoint</h2>
+          <p>{mockData.endpoint}</p>
+          <h2 className="text-xl">Secret Key</h2>
+          <p>{mockData.secretKey} <span className="text-sm text-red-500">[You are the only responsible for this key!]</span></p>
         </div>
-  
-        <div className="bg-white p-8 rounded-xl shadow-lg">
-          <h2 className="text-3xl font-semibold tracking-tighter mb-6">Usage</h2>
-          <BarChart
-              xAxis={[
-                {
-                  id: 'barCategories',
-                  data: ['06/09', '07/09', '08/09'],
-                  scaleType: 'band',
-                },
-              ]}
-              series={[
-                {
-                  data: [200, 500, 300],
-                },
-              ]}
-              width={300}
-              height={300}
-          />
+        <div className="space-y-4">
+          <div className="bg-blue-600 text-white p-6 rounded-lg shadow-md">
+            <h2 className="text-xl">Daily Usage</h2>
+            <p>{mockData.dailyUsage}</p>
+          </div>
+          <div className="bg-green-600 text-white p-6 rounded-lg shadow-md">
+            <h2 className="text-xl">Total Usage</h2>
+            <p>{mockData.totalUsage}</p>
+          </div>
         </div>
       </div>
       
-      <div className="bg-white p-8 mt-12 rounded-xl shadow-lg">
-        <h2 className="text-3xl font-semibold tracking-tighter mb-6">Total Usage</h2>
-        <p className="text-xl text-gray-600">{mockData.totalUsage} requests</p>
+      <div className="bg-white p-6 rounded-lg shadow-md">
+        <h2 className="text-xl mb-4">Logs</h2>
+        <ul>
+          {mockData.logs.map((log: LogType) => (
+            <li key={log.id} className="border-b border-gray-200 py-2">{log.message}</li>
+          ))}
+        </ul>
       </div>
     </div>
   )
