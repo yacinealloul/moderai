16a7722d4188eb046b4984948e115916f8222049
yacinealloul <40143057+yacinealloul@users.noreply.github.com>
2023-09-25 11:07:04 +0200

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,12 +3,13 @@ import React from 'react'
 const Footer = () => {
   return (
       <>
-          <footer className="container py-5 md:py-10 lg:py-20 bg-blue-900 text-white">
-              <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-                <h2 className="text-xl sm:text-2xl md:text-2xl lg:text-3xl font-bold mb-4">Contact Us</h2>
-                {/* ... */}
-              </div>
-          </footer>
+<section className="py-20 bg-blue-900 text-white">
+    <div className="container mx-auto px-4 text-center">
+        <h2 className="text-5xl font-extrabold mb-10">Contact us</h2>
+
+    </div>
+</section>
+
       </>
 
   )
--- components/Playground.tsx
+++ components/Playground.tsx
@@ -1,37 +1,62 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react';
 
-const Playground = () => {
-        const [response,setResponse] = useState('');
-        const [input,setInput] = useState('');
-    
-        return (
-            <>
-            <section className="py-20">
-                <div className="container mx-auto px-4 flex flex-col md:flex-row">
-                    <div className="w-full md:w-1/2 pr-4 md:pr-8 mb-8 md:mb-0">
-                        <h2 className="text-5xl font-extrabold mb-6">Playground</h2>
-                        <textarea className="w-full p-4 rounded-lg bg-white shadow-lg transition-all duration-500 hover:shadow-xl resize-none" rows={9} value={input} onChange={(e) => setInput(e.target.value)} />
-                        <button className="mt-4 p-3 rounded-lg text-white bg-gradient-to-r from-blue-900 to-blue-700 hover:from-blue-700 hover:to-blue-500 w-full">
-                            Test
+const Playground: React.FC = () => {
+    const [content, setContent] = useState('');
+    const [response, setResponse] = useState('');
+
+    const handleModerate = () => {
+        // Handle moderation of the content here
+        console.log('Content to moderate:', content);
+        // Mock response after moderation (You'd likely use an API here)
+        setResponse(`{
+  "status": "Moderated",
+  "message": "This is a safe content!"
+}`);
+    };
+
+    return (
+        <div className="min-h-screen bg-gray-100 py-20 px-6">
+            <div className="container mx-auto">
+                {/* Header */}
+                <div className="text-center mb-12">
+                    <div className="text-5xl font-extrabold mb-3">Playground</div>
+                    <div className="text-xl text-gray-600">Test and see results of your moderation API in real-time.</div>
+                </div>
+                
+                {/* Content Input and Controls */}
+                <div className="bg-white p-8 rounded-xl shadow-2xl flex flex-col md:flex-row gap-8 mb-12">
+                    <textarea
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        className="flex-grow bg-gray-900 text-white p-4 rounded-md shadow-inner resize-none"
+                        style={{ minHeight: '300px', fontFamily: 'monospace' }}
+                        placeholder="Paste the content you want to moderate here..."
+                    ></textarea>
+
+                    <div className="flex flex-col gap-4">
+                        <button
+                            onClick={handleModerate}
+                            className="py-3 px-6 bg-gradient-to-r from-blue-500 to-green-500 hover:from-blue-600 hover:to-green-600 transition duration-300 rounded-md text-white font-semibold shadow-md"
+                        >
+                            Moderate Content
                         </button>
-                    </div>
-                    <div className="w-full md:w-1/2 pl-4 md:pl-8">
-                        <h2 className="text-5xl font-extrabold mb-6">Response</h2>
-                        <div className="p-4 rounded-lg bg-blue-50 text-gray-900 h-full border-l-4 border-blue-900">
-                            <code>
-                                {response || `{
-            "response": "Your JSON will appear here."
-            }`}
-                            </code>
+
+                        <div className="text-gray-600 text-sm">
+                            Content will be checked for any violations as per the moderation guidelines.
                         </div>
                     </div>
                 </div>
-            </section>
-            </>
 
+                {/* Moderation Output */}
+                <div className="bg-white p-8 rounded-xl shadow-2xl">
+                    <div className="font-semibold mb-4">Moderation Output:</div>
+                    <pre className="bg-gray-900 text-white p-4 rounded-md shadow-inner" style={{ fontFamily: 'monospace', minHeight: '200px' }}>
+                        {response || 'Moderation results will be displayed here.'}
+                    </pre>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-            );
-}
-
-export default Playground
+export default Playground;
