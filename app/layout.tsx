bcc8ed4fa3bd7675c1b1fd787efc934c13390222
yacinealloul <40143057+yacinealloul@users.noreply.github.com>
2023-10-06 18:02:12 +0200

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,18 +8,27 @@ import { useAuth } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['greek'] })
 
+
 export const metadata: Metadata = {
-  title: 'Flagged.ai',
-  description: 'Streamline your workflow with advanced text processing and moderation.',
-  keywords: ["Text Moderation", "Content Filtering", "Moderation Automation", "AI for Moderation", "Online Moderation Tool", "Content Safety", "Real-time Moderation", "Comment Management", "Inappropriate Content Detection", "User-generated Content Control"],
-  openGraph:{
-      title: 'Flagged.ai',
-      description: 'Streamline your workflow with advanced text processing and moderation.',
-      images:['https://media.discordapp.net/attachments/1005203757275029606/1149279122682544178/0gvHYoi.png'],
-
+  title: 'Flagged.ai | API Moderation',
+  description: 'Use Flagged.ai for seamless content moderation in your application.',
+  
+  // Open Graph metadata
+  openGraph: {
+    title: 'Flagged.ai | Seamless API Moderation',
+    description: 'Integrate Flagged.ai for efficient content moderation solutions in your application.',
+    type: 'website',  // Can be other types like 'article', depending on your content
+    images: {
+      url: 'https://media.discordapp.net/attachments/1005203757275029606/1159879801658679437/image.png?ex=6532a13e&is=65202c3e&hm=aae1ed0e255daa17df58c946f4876470d67f58474bae808e017414a7e891576c&=&',  // Replace with your image path
+      alt: 'Description of the image',  // Useful for accessibility
+      width: 1200,  // Ideally at least 1200 pixels wide
+      height: 630,  // Ideally 630 pixels tall
+    },
+    url: 'https://flagged.ai',  // The canonical URL for your page
+    siteName: 'Flagged.ai',
+    locale: 'en_US',  // e.g., 'en_US' or 'fr_FR'
   }
-}
-
+};
 export default function RootLayout({
   children,
 }: {
