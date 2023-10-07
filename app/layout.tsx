03b7ab30499574c120b4391a8060d771e6d53d79
yacinealloul <40143057+yacinealloul@users.noreply.github.com>
2023-10-08 00:02:59 +0200

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,15 +26,15 @@ export default function RootLayout({
   <meta property="og:type" content="website"/>
   <meta property="og:title" content="Flagged.ai | Seamless API Moderation"/>
   <meta property="og:description" content="Integrate Flagged.ai for efficient content moderation solutions in your application."/>
-  <meta property="og:image" content="https://imgur.com/a/Njm0EDK"/>
-  <meta name="og:site_name" content="Flagged AI"/>
+  <meta property="og:image" content="https://media.discordapp.net/attachments/1005203757275029606/1160333006716805200/Untitled.png?ex=65344752&is=6521d252&hm=80ad23884fe24bcd0382f77058185365f645e381169975ad9afa821aa7221e4"/>
+  <meta property="og:site_name" content="Flagged AI"/>
 
   <meta name="twitter:card" content="summary_large_image"/>
   <meta property="twitter:domain" content="flagged.ai"/>
   <meta property="twitter:url" content="https://www.flagged.ai/"/>
   <meta name="twitter:title" content="Flagged.ai | Seamless API Moderation"/>
   <meta name="twitter:description" content="Integrate Flagged.ai for efficient content moderation solutions in your application."/>
-  <meta name="twitter:image" content="https://imgur.com/a/Njm0EDK"/>
+  <meta name="twitter:image" content="https://media.discordapp.net/attachments/1005203757275029606/1160333006716805200/Untitled.png?ex=65344752&is=6521d252&hm=80ad23884fe24bcd0382f77058185365f645e381169975ad9afa821aa7221e4"/>
 
         
       </head>
