a70941237fa2b1031dcd5288e5a49d7e5f18780d
yacinealloul <40143057+yacinealloul@users.noreply.github.com>
2023-10-07 12:00:29 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
   <meta property="og:title" content="Flagged.ai | Seamless API Moderation"/>
   <meta property="og:description" content="Integrate Flagged.ai for efficient content moderation solutions in your application."/>
   <meta property="og:image" content="https://media.discordapp.net/attachments/1005203757275029606/1159879801658679437/image.png?ex=6532a13e&is=65202c3e&hm=aae1ed0e255daa17df58c946f4876470d67f58474bae808e017414a7e891576c&=&"/>
-  <meta property="og:sitename" content="https://www.flagged.ai/"/>
+  <meta property="og:site_name" content="https://www.flagged.ai/"/>
 
   <meta name="twitter:card" content="summary_large_image"/>
   <meta property="twitter:domain" content="flagged.ai"/>
