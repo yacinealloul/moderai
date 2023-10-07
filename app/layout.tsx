56adaefe2e31ad8b149434669a869b7bb1eb40bd
yacinealloul <40143057+yacinealloul@users.noreply.github.com>
2023-10-07 12:04:07 +0200

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,26 +9,6 @@ import { useAuth } from '@clerk/nextjs'
 const inter = Inter({ subsets: ['greek'] })
 
 
-export const metadata: Metadata = {
-  title: 'Flagged.ai | API Moderation',
-  description: 'Use Flagged.ai for seamless content moderation in your application.',
-  
-  // Open Graph metadata
-  openGraph: {
-    title: 'Flagged.ai | Seamless API Moderation',
-    description: 'Integrate Flagged.ai for efficient content moderation solutions in your application.',
-    type: 'website',  // Can be other types like 'article', depending on your content
-    images: {
-      url: 'https://media.discordapp.net/attachments/1005203757275029606/1159879801658679437/image.png?ex=6532a13e&is=65202c3e&hm=aae1ed0e255daa17df58c946f4876470d67f58474bae808e017414a7e891576c&=&',  // Replace with your image path
-      alt: 'Description of the image',  // Useful for accessibility
-      width: 1200,  // Ideally at least 1200 pixels wide
-      height: 630,  // Ideally 630 pixels tall
-    },
-    url: 'https://flagged.ai',  // The canonical URL for your page
-    siteName: 'Flagged.ai',
-    locale: 'en_US',  // e.g., 'en_US' or 'fr_FR'
-  }
-};
 export default function RootLayout({
   children,
 }: {
@@ -47,7 +27,7 @@ export default function RootLayout({
   <meta property="og:title" content="Flagged.ai | Seamless API Moderation"/>
   <meta property="og:description" content="Integrate Flagged.ai for efficient content moderation solutions in your application."/>
   <meta property="og:image" content="https://media.discordapp.net/attachments/1005203757275029606/1159879801658679437/image.png?ex=6532a13e&is=65202c3e&hm=aae1ed0e255daa17df58c946f4876470d67f58474bae808e017414a7e891576c&=&"/>
-  <meta property="og:site_name" content="https://www.flagged.ai/"/>
+  <meta property="og:site_name" content="Flagged AI"/>
 
   <meta name="twitter:card" content="summary_large_image"/>
   <meta property="twitter:domain" content="flagged.ai"/>
