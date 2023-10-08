7044fe81b247b14e1539cdf8c6e800e7787de9b5
yacinealloul <40143057+yacinealloul@users.noreply.github.com>
2023-10-08 12:08:14 +0200

    Update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -89,14 +89,14 @@ export default function Home() {
           <div>
             <div className='mx-auto max-w-6xl px-6 lg:px-8'>
               <div className='mt-16 flow-root sm:mt-24'>
-                <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
+                <div className='-m-2 rounded-xl bg-gray-900/5 p-1 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
                   <Image
-                    src={'/image.jpg'}
+                    src={'/image.png'}
                     alt='product preview'
-                    width={1364}
-                    height={866}
+                    width={1080}
+                    height={720}
                     quality={100}
-                    className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
+                    className='rounded-md bg-transparent p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
                   />
                 </div>
               </div>
