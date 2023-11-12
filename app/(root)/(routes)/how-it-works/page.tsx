87a769e855f22f0c00883c28b3a290a8befe45fd
yacinealloul <40143057+yacinealloul@users.noreply.github.com>
2023-11-12 18:32:40 +0100

    update

--- app/(root)/(routes)/how-it-works/page.tsx
+++ app/(root)/(routes)/how-it-works/page.tsx
@@ -16,11 +16,11 @@ export default function HowItWorks() {
     <section className="relative pt-20 pb-32 flex flex-col items-center justify-center text-center">
         <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50 z-10">
             <p className="text-sm font-semibold text-gray-700">
-                Here&apos;s how it works 👋
+                How it works 👋
             </p>
         </div>
         <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl z-10">
-            Learn how <span className="text-blue-600">flagged.ai</span> can transform your approach to text processing.
+            How <span className="text-blue-600">flagged.ai</span> can transform your approach to text processing.
         </h1>
         <p className="mt-5 max-w-prose text-zinc-700 sm:text-lg z-10">
             Submit your text to our API endpoint, and let our system do its magic. Our model system analyzes text content in real-time and delivers detailed results within milliseconds.
